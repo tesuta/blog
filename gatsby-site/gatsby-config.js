909d65be4876c728d3141f821ac4ab8a1d92e592
tungping <voskaul@gmail.com>
2018-12-04 23:34:13 +0800

    gatsby test

--- gatsby-site/gatsby-config.js
+++ gatsby-site/gatsby-config.js
@@ -25,6 +25,9 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+    module.exports = {
+      pathPrefix: "/reponame",
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
